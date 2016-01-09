ef07b0954cc66e920b65ad9da5dd9816651a9176
Popa Bogdan Constantin <bogdan.popa@reea.net>
2016-01-09 19:43:38 +0200

    Added form submit to change a list

--- directory.js
+++ directory.js
@@ -5,5 +5,13 @@ angular.module('directoryApp', [])
                 {name: 'Vasile', age: 49},
                 {name: 'Trei', age: 25},
                 {name: 'Gicu', age: 33}
-            ]
+            ];
+
+            $scope.addPerson = function () {
+                //Add the object to the list.
+                $scope.list.push({name: $scope.name, age: $scope.age});
+                //Reset name and age after submit
+                $scope.name = '';
+                $scope.age = 0;
+            }
         });
